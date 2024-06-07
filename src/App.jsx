9b4fd0f6d987550453fa55e7a5e47c34fc5fa249
Guilherme Fontes <guilherme.fontes98@outlook.com>
2024-06-07 14:42:54 +0100

    imports and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import CompanyList from "./components/CompanyList";
+import RequestList from "./components/RequestList";
+import PersonalDetails from "./components/PersonalDetails";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/companies" component={CompanyList} />
+        <Route path="/requests" component={RequestList} />
+        <Route path="/personal-details" component={PersonalDetails} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
